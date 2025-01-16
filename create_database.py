377f8513f38fce4a06762a5ed80ceed9b7083fcd
Sofiia <lazareva.sone4ka@yandex.ru>
2025-01-16 17:44:57 +0100

    Added files for work with my own database

---
+++ create_database.py
@@ -0,0 +1,159 @@
+"""
+- do something
+"""
+
+
+import yaml
+import json
+import requests
+import psycopg2
+from psycopg2 import sql
+from psycopg2.extras import DictCursor
+from contextlib import closing
+
+
+with open('configuration/config_server.yaml', 'r') as handle:
+    config = yaml.full_load(handle)
+
+with open('database/translate_countries.json', encoding='utf-8') as handle:
+    dict_countries = json.load(handle)
+
+with open('database/translate_genres.json', encoding='utf-8') as handle:
+    dict_genres = json.load(handle)
+
+with open('configuration/config_database.yaml', 'r') as handle:
+    configs = yaml.full_load(handle)
+
+
+def load_from_api_by_query(query: dict) -> dict:
+    headers = query['headers']
+    url = query['url']
+    params = query['params']
+
+    response = requests.get(url, headers=headers, params=params)
+    response_json = response.json()
+    for page in range(2, response_json['pages'] + 1):
+        params['page'] = page
+        add_response = requests.get(url, headers=headers, params=params)
+        add_response_json = add_response.json()
+        response_json['docs'].extend(add_response_json['docs'])
+    return response_json
+
+
+def load_all_dataset():
+    query_1 = config['test_query']
+    response = load_from_api_by_query(query_1)['docs']
+    count_1 = len(response)
+    print(f'Loaded {count_1} from country {query_1["params"]["countries.name"]}. '
+          f'There are {count_1} tv-series in total.')
+
+    query_2 = query_1
+    query_2['params']['page'] = 1
+    query_2['params']['countries.name'] = 'Китай'
+    response.extend(load_from_api_by_query(query_2)['docs'])
+    count_2 = len(response)
+    print(f'Loaded {count_2 - count_1} from country {query_2["params"]["countries.name"]}. '
+          f'There are {count_2} tv-series in total.')
+
+    query_3 = query_1
+    query_3['params']['page'] = 1
+    query_3['params']['countries.name'] = 'Япония'
+    response.extend(load_from_api_by_query(query_3)['docs'])
+    count_3 = len(response)
+    print(f'Loaded {count_3 - count_2} from country {query_3["params"]["countries.name"]}. '
+          f'There are {count_3} tv-series in total.')
+
+    return response
+
+
+def create_different_datasets(dataset: list) -> (list, list, list):
+    series_values, countries_values, genres_values = [], [], []
+    pr_key = set()
+    for elem in dataset:
+        kp_id = elem['id']
+        if kp_id in pr_key:
+            continue
+        pr_key.add(kp_id)
+        # name and description
+        name = elem['name']
+        if name is None:
+            continue
+        description = elem['shortDescription']
+        if description is None:
+            description = elem['description']
+
+        # release year
+        production_year = elem['year']
+        # rating: kp and imdb
+        kp_rating = round(elem['rating']['kp'], 1)
+        imdb_rating = round(elem['rating']['imdb'], 1)
+
+        # genres to string
+        list_genres = [genre['name'] for genre in elem['genres']]
+        all_genres = ', '.join(list_genres)
+        # countries to string
+        list_countries = [country['name'] for country in elem['countries']]
+        all_countries = ', '.join(list_countries)
+
+        # create link
+        link = f'https://www.kinopoisk.ru/series/{str(kp_id)}/'
+
+        elem_series = (
+            kp_id
+            , name
+            , kp_rating
+            , imdb_rating
+            , production_year
+            , link
+            , description
+            , all_countries
+            , all_genres
+        )
+
+        elem_countries_bools = [country in list_countries for country in dict_countries['ru-en']]
+        elem_countries = (kp_id, *elem_countries_bools)
+
+        elem_genres_bools = [genre in list_genres for genre in dict_genres['ru-en']]
+        elem_genres = (kp_id, *elem_genres_bools)
+
+        series_values.append(elem_series)
+        countries_values.append(elem_countries)
+        genres_values.append(elem_genres)
+
+    return series_values, countries_values, genres_values
+
+
+def create_database_with_sql(values_series, values_countries, values_genres):
+    with closing(psycopg2.connect(**configs)) as conn:
+        with conn.cursor(cursor_factory=DictCursor) as cursor:
+            conn.autocommit = True
+
+            values_series_sql = sql.SQL(',').join(map(sql.Literal, values_series))
+            insert = sql.SQL(
+                'INSERT INTO tv_series '
+                'VALUES {}').format(values_series_sql)
+            cursor.execute(insert)
+
+            values_countries_sql = sql.SQL(',').join(map(sql.Literal, values_countries))
+            insert = sql.SQL(
+                'INSERT INTO countries '
+                'VALUES {}').format(values_countries_sql)
+            cursor.execute(insert)
+
+            values_genres_sql = sql.SQL(',').join(map(sql.Literal, values_genres))
+            insert = sql.SQL(
+                'INSERT INTO genres '
+                'VALUES {}').format(values_genres_sql)
+            cursor.execute(insert)
+
+
+# launching ------------------------------------------------------------------------------------------
+def launch_creating_datasets() -> None:
+    data_from_api = load_all_dataset()
+    series, countries, genres = create_different_datasets(data_from_api)
+    create_database_with_sql(series, countries, genres)
+    return None
+
+
+if __name__ == '__main__':
+    launch_creating_datasets()
---
+++ database_queries.py
@@ -0,0 +1,140 @@
+"""
+TODO: add description:
+ - QUERY -> DATABASE -> OUTPUT
+"""
+
+
+import yaml
+import json
+import requests
+import psycopg2
+from psycopg2 import sql
+from psycopg2.extras import DictCursor
+from contextlib import closing
+
+
+# load configurate files
+with open('configuration/config_server.yaml', 'r') as handle:
+    config = yaml.full_load(handle)
+
+with open('configuration/config_database.yaml', 'r') as handle:
+    configs = yaml.full_load(handle)
+
+# load files with translated inscriptions
+with open('database/translate_genres.json', encoding='utf-8') as handle:
+    dict_genres = json.load(handle)
+
+with open('database/translate_countries.json', encoding='utf-8') as handle:
+    dict_countries = json.load(handle)
+
+
+# TODO #2:
+#  - check the encoding (e.g.: https://www.kinopoisk.ru/series/5229364/)
+
+# QUERY -> DATABASE -> OUTPUT
+def random() -> dict:
+    # create database session
+    with closing(psycopg2.connect(**configs)) as conn:
+        with conn.cursor(cursor_factory=DictCursor) as cursor:
+            cursor.execute('SELECT t.* '
+                           'FROM tv_series AS t '
+                           'INNER JOIN countries AS c '
+                           'ON t.id = c.id '
+                           'WHERE (t.kp_rating >= 7 OR t.imdb_rating >= 9) AND c.south_korea = TRUE '
+                           'ORDER BY RANDOM() '
+                           'LIMIT 1;')
+
+            row = cursor.fetchall()
+            _, name, kp, imdb, production_year, link, description, countries, genres = row[0]
+            answer = {
+                'Name': name
+                , 'Description': description
+                , 'KP rating': kp
+                , 'IMDB rating': imdb
+                , 'Genres': genres
+                , 'Country': countries
+                , 'Release year': production_year
+                , 'Link': link
+            }
+            return answer
+
+
+def last() -> list[dict]:
+    with closing(psycopg2.connect(**configs)) as conn:
+        with conn.cursor(cursor_factory=DictCursor) as cursor:
+            cursor.execute('SELECT t.* '
+                           'FROM tv_series AS t '
+                           'JOIN countries AS c '
+                           'ON t.id = c.id '
+                           'WHERE c.south_korea = TRUE '
+                           'ORDER BY t.production_year DESC, t.kp_rating DESC '
+                           'LIMIT 5;')
+            answer = []
+            for row in cursor:
+                _, name, kp, imdb, production_year, link, description, countries, genres = row
+                new_elem = {
+                    'Name': name
+                    , 'Description': description
+                    , 'KP rating': kp
+                    , 'IMDB rating': imdb
+                    , 'Genres': genres
+                    , 'Country': countries
+                    , 'Release year': production_year
+                    , 'Link': link
+                }
+                answer.append(new_elem)
+            return answer
+
+
+def select(
+        usr_genre: str, usr_rating: str, usr_country: str, usr_year: str, usr_count: str
+        , usr_mode: str  # best or random
+) -> list[dict]:
+    # formulate conditions
+    condition_genre = ''
+    if usr_genre != 'любой':
+        genre = dict_genres['ru-en'][usr_genre]
+        condition_genre = f' AND g.{genre} = TRUE '
+
+    condition_rating = ''
+    if usr_rating != 'любая':
+        condition_rating = f' AND t.kp_rating >= {usr_rating} '
+
+    condition_year = ''
+    if usr_year != 'любой':
+        condition_year = f' AND t.production_year >= {usr_year} '
+
+    country_column = f'{dict_countries["ru-en"][usr_country]}'
+    count = int(usr_count)
+
+    condition_order = 'ORDER BY t.kp_rating DESC' if usr_mode == 'лучшие' else 'ORDER BY RANDOM()'
+
+    with closing(psycopg2.connect(**configs)) as conn:
+        with conn.cursor(cursor_factory=DictCursor) as cursor:
+            query = f"""
+                SELECT t.* 
+                FROM tv_series AS t 
+                JOIN countries c ON t.id = c.id 
+                JOIN genres AS g ON t.id = g.id 
+                WHERE c.{country_column} = TRUE 
+                {condition_genre} {condition_year} {condition_rating} 
+                {condition_order} 
+                LIMIT {count};
+            """
+            cursor.execute(query)
+
+            answer = []
+            for row in cursor:
+                _, name, kp, imdb, production_year, link, description, countries, genres = row
+                new_elem = {
+                    'Name': name
+                    , 'Description': description
+                    , 'KP rating': kp
+                    , 'IMDB rating': imdb
+                    , 'Genres': genres
+                    , 'Country': countries
+                    , 'Release year': production_year
+                    , 'Link': link
+                }
+                answer.append(new_elem)
+            return answer
